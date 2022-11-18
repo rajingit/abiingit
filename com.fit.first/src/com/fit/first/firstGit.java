a343bed6c1c5b484d97271e82925a78b1ef84982
rajingit <rajingm@gmail.com>
2022-11-18 11:27:52 -0600

    My First Git

---
+++ com.fit.first/src/com/fit/first/firstGit.java
@@ -0,0 +1,11 @@
+package com.fit.first;
+
+public class firstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Git");
+
+	}
+
+}
---
+++ com.fit.first/src/module-info.java
@@ -0,0 +1,2 @@
+module com.fit.first {
+}
